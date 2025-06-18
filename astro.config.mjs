7bf1bbf5e20b109e3e5ebd96da0dcbdf9f20de4c
Robin Dehouck <59605143+RobinDehouck@users.noreply.github.com>
2025-06-18 19:00:13 -0300

    Update Astro configuration

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+  site: 'https://test2.vercel.app',
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    mdx(),
+    sitemap(),
+  ],
+  vite: {
+    resolve: {
+      alias: {
+        '~': new URL('./src', import.meta.url).pathname,
+      },
+    },
+  },
+});
